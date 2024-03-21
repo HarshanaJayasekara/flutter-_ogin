79a7b0d0bcdb2537d4cc1132e4b66f67bf5eb975
Harshana Jayasekara <98016320+HarshanaJayasekara@users.noreply.github.com>
2024-03-21 22:20:12 +0530

    Register.Dart

---
+++ lib/register.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:login/widget/button.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({super.key});
+
+  @override
+  State<MyRegister> createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  var Password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/1.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(),
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 30, left: 35),
+              child: const Text(
+                "Welcome \n Back",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 33,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.2,
+                    left: 35,
+                    right: 35),
+                child: Column(
+                  children: [
+                    mm(pas: 'Name'),
+                    const SizedBox(height: 30),
+                    mm(pas: 'Email'),
+                    const SizedBox(height: 30),
+                    mm(pas: 'Password'),
+                    const SizedBox(height: 30),
+                    mm(pas: 'Confirm Password'),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xff4c505b),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.arrow_forward),
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconBtn(pageName: 'login', txtName: 'login'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
